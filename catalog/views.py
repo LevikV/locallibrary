3dfbe7b0f935b5dc41fe88d3675fcd2c06351fb1
LevikV <alex@kplus79.ru>
2020-07-10 13:20:50 +1000

    Добавил возможность авторизованным пользователям просматривать книги взятые в аренду

--- catalog/views.py
+++ catalog/views.py
@@ -51,6 +51,6 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     model = BookInstance
     template_name = 'catalog/bookinstance_list_borrowed_user.html'
     paginate_by = 10
-
+#Переопределяем вывод отфильтровав значения
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
